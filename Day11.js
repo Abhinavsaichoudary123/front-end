ed3c5a222d60bc728dae8484272efb901bf00a92
Abhinavsaichoudary123 <abhinavsaichoudary30@gmail.com>
2024-09-28 12:31:16 -0400

    Day11.js

---
+++ Day11.js
@@ -0,0 +1,19 @@
+var favoriteFood = "biryani"
+console.log(favoriteFood)
+var myAge=22
+console.log(myAge)
+var isStudent = "true"
+console.log(isStudent)
+var myPet =
+console.log(myPet)
+var mycar=null
+console.log(mycar)
+console.log("1"+"4"+"1") 
+console.log("A"-"B") 
+console.log("A"-"B"+"1") 
+console.log("A"-"B"+1) 
+console.log("1"-"1") 
+console.log(+"1"+"1"+"1")
+console.log(1 + + "1" + "1") 
+console.log(1 - "1" + "1")
+console.log(+ "")
